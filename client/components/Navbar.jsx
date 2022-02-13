8c9fc3cb3d33e914a8a1c439fd03c30cbfdb3596
v4rsh1th <v4rsh1th@gmail.com>
2022-02-13 13:44:26 +0530

    Getting done with Navbar.

---
+++ client/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { CgMenuRightAlt } from "react-icons/cg";
+import { CgCloseO } from "react-icons/cg";
+
+import styles from '../styles/Home.module.css'
+import logo from '../public/logo.svg';
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <Image src={logo} alt="logo" className="w-32 cursor-pointer" />
+      </div>
+      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+        <li>
+          <Link href="/market">
+            <a>Market</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/NFT">
+            <a>NFT</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/creators">
+            <a>Creators</a>
+          </Link>
+        </li>
+        <li className={styles.connect_btn_navbar}>
+          Connect to Wallet
+        </li>
+      </ul>
+      <div className="flex relative">
+        {!toggleMenu && (
+          <CgMenuRightAlt fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
+        )}
+        {toggleMenu && (
+          <CgCloseO fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
+        )}
+        {toggleMenu && (
+          <ul
+            className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+          >
+            <li className="text-xl w-full my-4"><CgCloseO onClick={() => setToggleMenu(false)} /></li>
+            <li>
+              <Link href="/market">
+                <a>Market</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/NFT">
+                <a>NFT</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/creators">
+                <a>Creators</a>
+              </Link>
+            </li>
+            <li className={styles.connect_btn_navbar_sm}>
+              Connect to Wallet
+            </li>
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ client/pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+import Welcome from '../components/Welcome'
+import Footer from '../components/Footer'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>ApexChain</title>
+        <meta name="description" content="ApexChain" />
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+
+      <div className="min-h-screen">
+        <div className={styles.gradient_bg_welcome}>
+          <Navbar />
+          <Welcome />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
