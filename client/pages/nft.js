cd6bb758cf1f29bd20763acc208dfd793b2bd15e
v4rsh1th <v4rsh1th@gmail.com>
2022-02-13 23:37:27 +0530

    Pages addition.

--- client/pages/nft.js
+++ client/pages/nft.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Creators() {
+export default function Nft() {
   return (
     <div className={styles.creators_page}>
       <Head>
-        <title>ApexChain &mdash; Creators</title>
+        <title>ApexChain &mdash; NFT</title>
         <meta name="description" content="ApexChain" />
         <link rel="icon" href="/logo.svg" />
       </Head>
