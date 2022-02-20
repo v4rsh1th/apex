d104f09b9973e687fa34c3f6a5645bd8cd380c7a
v4rsh1th <v4rsh1th@gmail.com>
2022-02-20 18:02:30 +0530

    removed the dirt in contract.

--- system/README.md
+++ system/README.md
@@ -1,15 +1,7 @@
-# Basic Sample Hardhat Project
+### Smart contract under ApexChain
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+This demonstrates the functionality of sending and receiving using the Hardhat usecase.
 
-Try running some of the following tasks:
+#### shell ->
 
-```shell
-npx hardhat accounts
-npx hardhat compile
-npx hardhat clean
-npx hardhat test
-npx hardhat node
-node scripts/sample-script.js
-npx hardhat help
-```
+```npx hardhat compile```
--- system/contracts/Transactions.sol
+++ system/contracts/Transactions.sol
@@ -5,24 +5,22 @@ pragma solidity ^0.8.0;
 contract Transactions {
   uint256 transactionCount;
 
-  event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+  event Transfer(address from, address receiver, uint amount, uint256 timestamp);
 
   struct TransferStruct {
     address sender;
     address receiver;
     uint amount;
-    string message;
     uint256 timestamp;
-    string keyword;
   }
 
   TransferStruct[] transactions;
 
-  function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+  function addToBlockchain(address payable receiver, uint amount) public {
     transactionCount += 1;
-    transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+    transactions.push(TransferStruct(msg.sender, receiver, amount, block.timestamp));
 
-    emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+    emit Transfer(msg.sender, receiver, amount, block.timestamp);
   }
 
   function getAllTransactions() public view returns (TransferStruct[] memory) {
