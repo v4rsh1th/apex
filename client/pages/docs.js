519d7299871648e394de2687e504aa5aa7273b09
v4rsh1th <v4rsh1th@gmail.com>
2022-02-22 11:29:43 +0530

    Docs gettin started.

--- client/pages/docs.js
+++ client/pages/docs.js
@@ -8,6 +8,7 @@ export default function Docs() {
       <Head>
         <title>ApexChain &mdash; Docs</title>
         <meta name="description" content="ApexChain" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"></link>
         <link rel="icon" href="/logo.svg" />
       </Head>
       <div className={styles.docs_content}>
@@ -17,10 +18,15 @@ export default function Docs() {
         <div id={styles.docs_title}>
           ApexChain // Docs
         </div>
-        <div id={styles.docs_container}>
-
+        <div className={styles.docs_container}>
+          <p style={{ "fontFamily": "Sanchez", "textDecoration": "underline", "cursor": "pointer" }} >
+            <a href="https://github.com/v4rsh1th/apex" target={"_blank"} rel="noopener noreferrer" >Contribute on GitHub <i className="bi bi-github"></i></a>
+          </p><br />
+          <p>
+            <strong>ApexChain</strong> is Open Source under MIT License and is open for everyone to contribute.
+          </p>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
