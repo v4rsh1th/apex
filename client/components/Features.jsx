63a007157a983fcc3c730e3fb04a8871c65404ed
v4rsh1th <v4rsh1th@gmail.com>
2022-02-23 23:46:35 +0530

    Footer!

--- client/components/Features.jsx
+++ client/components/Features.jsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '../public/logo.svg';
 import styles from '../styles/Home.module.css';
 
 const Features = () => {
@@ -31,7 +34,7 @@ const Features = () => {
               Fast
             </div>
             <div className={styles.qualities_description}>
-              There are no dependencies—settlement houses, card processors or banks. All ApexChain transactions are confirmed in 10 - 20 seconds.
+              There are no dependencies—settlement houses, card processors or banks. All ApexChain transactions are confirmed in 10 - 40 seconds.
             </div>
           </div>
         </div>
@@ -47,8 +50,37 @@ const Features = () => {
         </div>
       </div>
 
-      <div className={styles.footer_container}>
-        afll
+      <div className={styles.footer}>
+        <div className={styles.footer_container_left}>
+          <div id={styles.footer_logo_name}>
+            <Image src={logo} alt="logo" className="cursor-pointer" width={"70px"} quality={100} />
+            <h1 className={styles.title}>
+              ApexChain
+            </h1>
+          </div>
+          <br /> <br />
+          <div id={styles.footer_brag}>
+            THE EASIEST WAY TO SEND AND SPEND
+          </div>
+        </div>
+
+        <div className={styles.footer_container_right}>
+          <p>
+            <strong>ApexChain</strong> is a platform, not a bank.
+          </p>
+          <p>
+            Open Source // MIT License
+          </p>
+          <p style={{ "fontFamily": "Sanchez", "textDecoration": "underline" }}>
+            <Link href="/license">
+              <a>LICENSE <i style={{ "fontSize": "13px" }} className="bi bi-arrow-up-right"></i></a>
+            </Link>
+          </p>
+          <br />
+          <p style={{ "fontFamily": "Sanchez", "textDecoration": "underline", "cursor": "pointer" }} >
+            <a href="https://github.com/v4rsh1th/apex" target={"_blank"} rel="noopener noreferrer" >Contribute on GitHub <i className="bi bi-github"></i></a>
+          </p>
+        </div>
       </div>
     </div>
   );
