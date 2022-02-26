2c158c3b36949a9d60559ea3bb019fd1ce62508f
v4rsh1th <v4rsh1th@gmail.com>
2022-02-26 11:00:26 +0530

    Bug fix (exc after initiating the transaction)

--- context/TransactionContext.jsx
+++ context/TransactionContext.jsx
@@ -5,13 +5,17 @@ import { contractABI, contractAddress } from "../utils/constants";
 export const TransactionContext = React.createContext();
 
 if (typeof window !== "undefined") {
-  window.ethereum
+  window.ethereum;
 }
 
 const getEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum);
   const signer = provider.getSigner();
-  const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
+  const transactionContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
   return transactionContract;
 }
 
@@ -54,7 +58,7 @@ export const TransactionProvider = ({ children }) => {
     }
   }
 
-  const sendTransaction = async () => {
+  const sendTransaction = async (connectedAccount = currentAccount) => {
     try {
       if (!ethereum) return alert("Please install Metamask.");
 
@@ -65,25 +69,66 @@ export const TransactionProvider = ({ children }) => {
       await ethereum.request({
         method: "eth_sendTransaction",
         params: [{
-          from: currentAccount,
+          from: connectedAccount,
           to: addressTo,
-          gas: "0x5208", // 21000 GWEI
-          value: parsedAmount._hex, // 0.00001
+          gas: "0x5208",
+          value: parsedAmount._hex,
         }]
       });
 
-      const transactionHash = await transactionContract.addToBlockchain(addressTo, parsedAmount);
+      const transactionHash = await transactionContract.addToBlockchain(
+        addressTo,
+        parsedAmount,
+        `Transferring ETH ${parsedAmount} to ${addressTo}`,
+      );
 
       setIsLoading(true);
       console.log(`Loading - ${transactionHash.hash}`);
+
       await transactionHash.wait();
+
+      // Sanity fn
+      // await saveTransaction(
+      //   transactionHash.hash,
+      //   amount,
+      //   connectedAccount,
+      //   addressTo
+      // )
+
       setIsLoading(false);
       console.log(`Success - ${transactionHash.hash}`);
     } catch (error) {
       console.error(error);
-      throw new Error("No ethereum object.");
+      // throw new Error("No ethereum object.");
     }
   }
+
+  // const saveTransaction = async (txHash, amount, fromAddress = currentAccount, toAddress) => {
+  //   const txDoc = {
+  //     _type: "transactions",
+  //     _id: txHash,
+  //     fromAddress: fromAddress,
+  //     toAddress: toAddress,
+  //     timestamp: new Date(Date.now()).toISOString(),
+  //     txHash: txHash,
+  //     amount: parseFloat(amount)
+  //   }
+  //   await client.createIfNotExists(txDoc)
+
+  //   await client
+  //     .patch(currentAccount)
+  //     .setIfMissing({ transactions: [] })
+  //     .insert('after', 'transactions[-1]', [
+  //       {
+  //         _key: txHash,
+  //         _ref: txHash,
+  //         _type: "reference"
+  //       },
+  //     ])
+  //     .commit()
+
+  //   return
+  // }
 
   useEffect(() => {
     checkIfWalletIsConnected();
