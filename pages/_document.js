4577a64ddb22db65e711ddbea1d593adf121552e
v4rsh1th <v4rsh1th@gmail.com>
2022-02-26 21:12:43 +0530

    _document full send. joke.

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,6 @@ export default function Home() {
         <link rel="icon" href="/logo.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"></link>
       </Head>
       <div className="min-h-screen">
         <div className={styles.gradient_bg_welcome}>
