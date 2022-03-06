fe05e3a529fb64b1f13a990f45eeb61810c0044f
v4rsh1th <v4rsh1th@gmail.com>
2022-03-06 11:27:22 +0530

    Internationalization.

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" />
       </Head>
